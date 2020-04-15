89801e1c6a11d5fa55ab7d39a37637e77a847a25
Jorti1 <phtcextara@gmail.com>
2020-04-15 15:40:47 -0700

    darkMode Update 2

--- js/script.js
+++ js/script.js
@@ -13,13 +13,3 @@
             .toggle('is-active');
     }
 })();
-
-$( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark" )) {
-      $( "body" ).removeClass( "dark" );
-      $( ".inner-switch" ).text( "OFF" );
-    } else {
-      $( "body" ).addClass( "dark" );
-      $( ".inner-switch" ).text( "ON" );
-    }
-});
